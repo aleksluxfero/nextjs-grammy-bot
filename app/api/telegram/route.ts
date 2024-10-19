85cc9ccebb00c3af8dbcf8626c82be2d90d98336
Alex <alex.luxfero@gmail.com>
2024-10-19 21:22:10 +0500

    test

--- app/api/telegram/route.ts
+++ app/api/telegram/route.ts
@@ -1,6 +1,10 @@
 import bot from '../../../bot';
 import {NextRequest} from "next/server";
 
+export async function GET() {
+  return Response.json({ message: "Hello" })
+}
+
 export async function POST(req: NextRequest) {
   const update = await req.json();  // Получаем обновление от Telegram
   await bot.handleUpdate(update);   // Передаем обновление боту
