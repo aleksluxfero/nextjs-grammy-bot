2da52d3e8054152e37db45025704a51eed342f9f
Alex <alex.luxfero@gmail.com>
2024-10-19 22:27:24 +0500

    new code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: { // [!code ++]
+        serverComponentsExternalPackages: ['grammy'], // [!code ++]
+    },
+};
 
 export default nextConfig;
